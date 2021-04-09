3dc3f48f5229fac8fd5c69cfdb5971a5fc239e89
杨腾飞 <yangtengfei@99elink.com>
2021-04-09 10:32:18 +0800

    修改

---
+++ src/MyFunc/src/Tools/Myfunc.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Flyty\MyFunc\Tools;
+
+
+class Myfunc
+{
+    public function add(){
+        return 'MyFunc add';
+    }
+}
---
+++ src/tools/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Flyty\Mytools\tools;
+
+
+class Test
+{
+    public function add(){
+        return 'test';
+    }
+}
